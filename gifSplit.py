0b859bf8516ff43315c89a6ac66cdb3f4d76d027
Emberstar <969242373@qq.com>
2023-03-29 20:38:17 +0800

    精简代码，附带示例图片

--- gifSplit.py
+++ gifSplit.py
@@ -1,13 +1,15 @@
 import os
 import argparse
-from PIL import Image, ImageSequence
+from PIL import Image, ImageSequence, ImageFilter
 
-parser = argparse.ArgumentParser(description='gif tool,used for showing picture or animate on oled')
-parser.add_argument('-g2j', '--gif2jpg', action="store_true",help='run gif2jpg （gif拆分成图片）')
-parser.add_argument('-j2gj', '--jpg2gray_jpg', action="store_true",help='run jpg2gray jpg （单张图片转灰度图）')
-parser.add_argument('-g2gj', '--gif2gray_jpg', action="store_true",help='run gif2gray_jpg （gif拆分成灰度图）')
+parser= argparse.ArgumentParser(description='gif tool,used for showing picture or animate on oled')
+parser.add_argument('-g2j', '--gif2jpg', action="store_true",help='run gif2jpg （gif拆分成图片，大小为gif的大小）')
+parser.add_argument('-j2j', '--jpg2jpg', action="store_true",help='run jpg2gray jpg （单张图片转灰度图）')
 parser.add_argument('-i','--img', type=str,help='img path （这里填需要转换的图片路径）')
-parser.add_argument('-s','--size', type=str,default='1280x640',help='default=1280x640 （默认大小为1280x640）')
+parser.add_argument('-s','--size', type=str,default='0x0',help='图像大小(默认为原图大小)')
+parser.add_argument('-m','--mode', type=str,default='RGB',help='default=RGB （图像模式默认为RGB，可选RGB|L|1）')
+parser.add_argument('-f','--fill', type=int,default=0,help='default=0 （是否填充，0不填充|1黑色|2白色）')
+
 
 args = parser.parse_args()
 
@@ -16,54 +18,58 @@ tw = int(target_size[0])
 th = int(target_size[1])
 
 img_path = args.img
+mode = args.mode
+fill = args.fill
 
 
 def gif2jpg():
     print('runing gif2jpg...')
     gif = img_path.split('.')[0]
 
-    path = gif+'_gif2jpg'
+    path = gif+'_g2j'
 
-    #获取gif迭代流
     im = Image.open(img_path)
     iter = ImageSequence.Iterator(im)
     
-    # 创建文件夹
     try:
         os.mkdir(path)
     except:
         pass
 
-    #图片转换，放入文件夹
+    
     try:
-
         for im in iter:
             current = im.tell()
-            width = im.size[0]   # 获取宽度
-            height = im.size[1]   # 获取高度
-            scale = max(width / tw, height / th)
+            width = im.size[0]
+            height = im.size[1]
+
+            if tw != 0 and th != 0:
+                scale = max(width / tw, height / th)
+                nw = int(width / scale +0.5)
+                nh = int(height / scale + 0/5)
+                im = im.resize((nw, nh))
             
-            nw = int(width / scale +0.5)
-            nh = int(height / scale + 0/5)
+            if mode.upper() == "1":
+                im = im.convert("L")
+                im = im.point(lambda x: 0 if x<128 else 255, '1')
+            else:
+                im = im.convert(mode.upper())
             
-            im = im.resize((nw, nh))
-            im = im.convert('1')
+            if fill:
+                new_image = Image.new(mode, (tw,th), 0)
+                new_image.paste(im, ((tw-nw) // 2,(th-nh) // 2))
+                im = new_image
             
-            new_image = Image.new('1', (tw,th), 0)
-            new_image.paste(im, ((tw-nw) // 2,(th-nh) // 2))
-            
-            image = new_image
-            image.save(path+'/'+str(current)+'.jpg')
+            im.save(path+'/'+str(current)+'.jpg')
+
     except TypeError as error:
         print(error)
 
 
-
-
-def jpg2gray_jpg(size):
-    print('runing jpg2gray_jpg...')
+def jpg2jpg():
+    print('runing jpg2jpg...')
     jpg = img_path.split('.')[0]
-    path = jpg+'_jpg2gray'
+    path = jpg+'_j2j'
     
     try:
         os.mkdir(path)
@@ -72,67 +78,41 @@ def jpg2gray_jpg(size):
     
     try:
         im = Image.open(img_path)
-        width = im.size[0]   # 获取宽度
-        height = im.size[1]   # 获取高度
-        scale = max(width / tw, height / th)
+        width = im.size[0]
+        height = im.size[1]
         
-        nw = int(width / scale +0.5)
-        nh = int(height / scale + 0/5)
+        if tw != 0 and th != 0:
+            if fill:
+                scale = max(width / tw, height / th)
+                nw = int(width / scale +0.5)
+                nh = int(height / scale + 0/5)
+                im = im.resize((nw, nh))
+            else:
+                im = im.resize((tw, th))
+
+        if mode.upper() == "1":
+                im = im.convert("L")
+                im = im.point(lambda x: 0 if x<128 else 255, '1')
+        else:  
+            im = im.convert(mode.upper())
         
-        im = im.resize((nw, nh))
+        if fill:
+            new_image = Image.new('1', (tw,th), 0)
+            new_image.paste(im, ((tw-nw) // 2,(th-nh) // 2))
+            im = new_image
+            
         
-        new_image = Image.new('1', (tw,th), 0)
-        new_image.paste(im, ((tw-nw) // 2,(th-nh) // 2))
-        
-        im = new_image
-        im = im.convert('1')
         im.save(path+'/'+jpg+'.jpg')
     except TypeError as error:
         print(error)
 
-def gif2gray_jpg(size):
-    print('runing gif2gray_jpg...')
-    gif = img_path.split('.')[0]
-    
-    path = gif+"_gif2gray_jpg"
-    
-    #获取gif迭代流
-    im = Image.open(img_path)
-    iter = ImageSequence.Iterator(im)
-    
-    try:
-        os.mkdir(path)
-    except:
-        pass
-    
-    try:
+
         
-        for im in iter:
-            current = im.tell()
-            width = im.size[0]   # 获取宽度
-            height = im.size[1]   # 获取高度
-            scale = max(width / tw, height / th)
-            
-            nw = int(width / scale +0.5)
-            nh = int(height / scale + 0/5)
-            
-            im = im.resize((nw, nh))
-            
-            new_image = Image.new('1', (tw,th), 0)
-            new_image.paste(im, ((tw-nw) // 2,(th-nh) // 2))
-            
-            im = new_image
-            image = im.convert('1')
-            image.save(path+'/'+str(current)+'.jpg')
-    except TypeError as error:
-        print(error)
-    
 if __name__ == '__main__':
+    print(args)
     if args.gif2jpg:
         gif2jpg()
-    if args.jpg2gray_jpg:
-        jpg2gray_jpg(args.size)
-    if args.gif2gray_jpg:
-        gif2gray_jpg(args.size)
+    if args.jpg2jpg:
+        jpg2jpg()
 
     print('finish')
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 # gifSplit
 
-这是一个可以将gif拆分成图片的脚本，功能有gif拆分图片、单张图片转灰度图、gif拆分成图片并转存灰度图
+这是一个可以将gif拆分成图片的脚本，功能有
+
+- gif拆分jpg：模式有RGB、灰度图像和二值图像，可以修改图片大小。
+- jpg转jpg：模式有RGB、灰度图像和二值图像，可以修改图片大小
 
 # 使用方法
 
@@ -9,31 +12,42 @@
 ```
 H:\gif-split>python gifSplit.py -h
 
-usage: gifSplit.py [-h] [-g2j] [-j2gj] [-g2gj] [-i IMG] [-s SIZE]
+usage: gifSplit.py [-h] [-g2j] [-j2j] [-i IMG] [-s SIZE] [-m MODE] [-f FILL]
 
 gif tool,used for showing picture or animate on oled
 
 optional arguments:
   -h, --help            show this help message and exit
-  -g2j, --gif2jpg       run gif2jpg （gif拆分成图片）
-  -j2gj, --jpg2gray_jpg
-                        run jpg2gray jpg （单张图片转灰度图）
-  -g2gj, --gif2gray_jpg
-                        run gif2gray_jpg （gif拆分成灰度图）
+  -g2j, --gif2jpg       run gif2jpg （gif拆分成图片，大小为gif的大小）
+  -j2j, --jpg2jpg       run jpg2gray jpg （单张图片转灰度图）
   -i IMG, --img IMG     img path （这里填需要转换的图片路径）
-  -s SIZE, --size SIZE  default=1280x640 （默认大小为1280x640）
+  -s SIZE, --size SIZE  图像大小(默认为原图大小)
+  -m MODE, --mode MODE  default=RGB （图像模式默认为RGB，可选RGB|L|1）
+  -f FILL, --fill FILL  default=0 （是否填充，0不填充|1黑色|2白色）
 ```
 
 ## 使用示例
 
+## gif2jpg
+
 ```
-python gifSplit.py -g2gj -i gif_path
+python gifSplit.py -g2j -i bad_apple.gif -s 1280x640 -m 1 -f 1
 ```
 
-意思是使用gif拆分成灰度图功能，将gif拆分成灰度图，大小默认为1280x640，原图比例保持不变，多出来的地方会用**黑色填充**。
+- 意思是将gif拆分成图片，文件为bad_apple.gif，大小为1280x640，原图比例保持不变，多出来的地方会用**黑色填充**，模式为二值图像，保存至文件夹**bad_apple_g2j**。
+- 注意如果是在SSD1306上使用必须要和屏幕分辨率**成任意比例关系**，比如128x64的分辨率可以用640x320、1280x640的图片，否则也不是不可以运行，只是oled上显示的图片将会被拉伸。
 
-如果要**改成其他大小**再加上参数`-s 128x64`即可。
+## jpg2jpg
 
-如果不需要填充，**只拆分GIF**，在参数-s 后加上原图片的大小即可。
+```
+python gifSplit.py -j2j -i head.jpg -m L
+```
 
-注意如果是在SSD1306上使用必须要和屏幕分辨率**成比例关系**，比如128x64的分辨率可以用1280x640的图片，否则也不是不可以运行，只是oled上显示的图片将会被拉伸。
+- 意思是处理jpg图片，文件是head.jpg，大小为原图大小，模式为灰度图像，不填充，保存至文件夹**head_j2j**。
+
+```
+python gifSplit.py -j2j -i head.jpg -s 1000x550 -m RGB
+```
+
+- 意思是处理jpg图片，文件是head.jpg，大小为原图1000x550，模式为RGB图像，不填充，保存至文件夹**head_j2j**。
+- 由于1000x550和原图比例不一致，原图将会被拉伸。
